c73c6d2efddc8b6e8849bb1e967478f4d53f7267
kingkihoon94 <kh06089@naver.com>
2021-05-20 14:55:14 +0900

    branch day002 create

---
+++ frontend/src/views/account/signUp.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <strong>회원가입 페이지</strong>
+        <form @submit.prevent="submitForm">
+            <div>
+                <label for="email">ID</label>
+                <input type="text" id="email" v-model="email"/>
+            </div>
+            <div>
+                <label for="password">비밀번호</label>
+                <input type="password" id="pw" v-model="pw"/>
+            </div>
+            <div>
+                <label for="nickname">이름</label>
+                <input type="text" id="nickname" v-model="nickname"/>
+            </div>      
+            <button type="submit">회원가입</button>
+        </form>
+    </div>   
+</template>
+
+<script>
+import {registerUser} from '@/api/index';
+    export default {
+        data() {
+            return {
+                email: '',
+                pw: '',
+                nickname: '',
+            };
+        },
+        methods: {
+            async submitForm() {
+                const userData = {
+                    email: this.email,     
+                    pw: this.pw,
+                    nickname: this.nickname,
+                };
+                const { data } = await registerUser(userData);
+                console.log(data.email);
+                console.log(data.nickname);
+                //this.logMessage = '${data.id} 님이 가입되었습니다.';
+
+                this.initForm();
+            },
+            initForm(){
+                this.email='';
+                this.pw='';
+                this.nickname='';
+            },
+            
+        },
+    }
+</script>
+
+<style scoped>
+</style>
