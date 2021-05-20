413d8705be3cb40fcef12fe1ae12d7afc6f2b30d
kingkihoon94 <kh06089@naver.com>
2021-05-20 14:54:29 +0900

    day002 branch

--- frontend/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-function registerUser(userData){
-    console.log(userData);
-    const url = 'http://localhost:8080/account/signUp';
-    return axios.post(url, userData);
-}
-
-export { registerUser };
