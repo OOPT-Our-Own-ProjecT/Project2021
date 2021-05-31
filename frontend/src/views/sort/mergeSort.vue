760cd8a4904c2dc476025e68dc4600f1489a59ec
kingkihoon94 <kh06089@naver.com>
2021-05-31 15:08:23 +0900

    complete mergesort

--- frontend/src/views/sort/mergeSort.vue
+++ frontend/src/views/sort/mergeSort.vue
@@ -9,7 +9,7 @@
         <div>
             <textarea v-model="data"></textarea>
             <button @click="push(data)">push</button>
-            <button @click="mergebutton()">merge</button>
+            <button @click="start()">start</button>
             <button @click="erase()">erase</button>
         </div>
         <div>
@@ -19,8 +19,11 @@
                 </div>
             </div>
             <br>
-            <div class="list2">
-                <div class="listdata" v-for="(item,idx) in merge_list"  v-bind:key=idx>
+            <div class="list2" v-show="divide_list.length > 0">
+                <button v-show="can_merge == false && can_end == false" @click="divide()">divide</button>
+                <button v-show="can_merge == true && can_end == false" @click="merge()">merge</button>
+                <h2 v-show="can_end == true"> MergeSort Finish!! </h2>
+                <div v-for="(item,idx) in divide_list"  v-bind:key=idx>
                     <div><h2>{{item}}</h2></div>
                 </div>
             </div>
@@ -43,12 +46,13 @@ export default {
 
     },
 
-
 	data () {
 		return {
             data: "" ,
             origin_list:[],
-            merge_list:[],
+            divide_list:[],
+            can_merge: false,
+            can_end: false,
         }
     },
     created(){
@@ -60,23 +64,52 @@ export default {
             else if(this.origin_list.length >=7) alert("OverFlow!!")
             else this.origin_list.push(parseFloat(data))
             this.data = ""
+            this.divide_list = []
+            this.can_end = false
         },
-        mergebutton(){
-            this.merge_list = []
-            this.merge_list = this.mergeSort(this.origin_list)
-        },
-        mergeSort(array){
-            if (array.length == 1){
-                return array
+        start(){
+            const tmp_list = []
+            for(var i=0; i<this.origin_list.length; i++){
+                tmp_list.push(this.origin_list[i])
             }
-            const middleIndex = Math.floor(array.length / 2);
-            const left = array.slice(0, middleIndex);
-            const right = array.slice(middleIndex);
-            const tmp_data =  this.merge(this.mergeSort(left), this.mergeSort(right));
-            this.merge_list.push(tmp_data)
-            return tmp_data
+            this.divide_list.push(tmp_list)
         },
-        merge(left, right){
+        divide(){
+            const tmp_list = []
+            for(var i=0; i<this.divide_list.length; i++){
+                const divide_data = this.divide_list[i]
+                if(divide_data.length ==1) tmp_list.push(divide_data)
+                else{
+                    const middleIndex = Math.floor(divide_data.length / 2);
+                    const left = divide_data.slice(0,middleIndex)
+                    const right = divide_data.slice(middleIndex, divide_data.length)
+                    tmp_list.push(left)
+                    tmp_list.push(right)
+                }
+            }//end for.
+            this.divide_list = tmp_list
+            const can_merge = this.all_divide()
+            if(can_merge) this.can_merge = true
+        },
+        merge(){
+            const tmp_list = []
+            for(var i=0; i<this.divide_list.length; i+=2){
+                if(i==this.divide_list.length-1) tmp_list.push(this.divide_list[i])
+                else tmp_list.push(this.mergesort(this.divide_list[i] , this.divide_list[i+1]))
+            }
+            this.divide_list = tmp_list
+            if(this.divide_list.length == 1){
+                this.can_merge = false
+                this.can_end = true
+            }
+        },
+        all_divide(){
+            for(var i=0; i<this.divide_list.length; i++){
+                if(this.divide_list[i].length != 1) return false
+            }
+            return true
+        },
+        mergesort(left, right){
             const result = [];
             while(left.length!=0 && right.length!=0){
                 left[0] <= right[0] ? result.push(left.shift()) : result.push(right.shift());
@@ -86,7 +119,8 @@ export default {
         erase(){
             this.data = ""
             this.origin_list = []
-            this.merge_list = []
+            this.divide_list = []
+            this.can_end = false
         },
     }
 }
@@ -102,7 +136,7 @@ export default {
     }
     .list2{
         margin: auto;
-        height: 100px;
+        height: 350px;
         width: 90%;
         border: 1px solid;
         padding: 15px;
